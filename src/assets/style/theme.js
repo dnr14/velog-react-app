f3fb20bb7e7a5f66e9931d4b6d73cb7bec706b46
dnr14 <dnr14@naver.com>
2021-11-03 00:58:04 +0900

    [feat] global style 및 theme 추가

---
+++ src/assets/style/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  insert: {},
+  update: {},
+  main: {
+    color: {},
+    media: {
+      tab: `@media screen and (max-width: 767px)`,
+      pc1: `@media screen and (max-width: 1056px)`,
+      pc2: `@media screen and (max-width: 1440px)`,
+      pc3: `@media screen and (max-width: 1919px)`,
+    },
+  },
+};
+
+export default theme;
